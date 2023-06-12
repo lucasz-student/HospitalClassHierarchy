aec11c95cb58ca44c5d9ed152fd1cc81b6b8ae33
lucasz-student <l.hsieh.student@gmail.com>
2023-06-12 17:14:26 -0500

    made unique exception unique

--- src/main/java/com/solvd/laba/Exceptions/ToolPermissionDeniedException.java
+++ src/main/java/com/solvd/laba/Exceptions/ToolPermissionDeniedException.java
@@ -1,17 +1,30 @@
 package com.solvd.laba.Exceptions;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+
+import com.solvd.laba.Utils.Utils;
+
 public class ToolPermissionDeniedException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 	
 	public ToolPermissionDeniedException() {
-	}
+		}
 	
 	public ToolPermissionDeniedException(String message) {
 		super(message);
+		Utils.logger.warn(message);
 	}
 	
 	public ToolPermissionDeniedException(String message, Throwable cause) {
 		super(message, cause);
+		Utils.logger.warn(message);
+	}
+	
+	public ToolPermissionDeniedException(String message, Object object) {
+		super(message);
+		Utils.logger.warn(Object.class + " Tried to equip Tool");
 	}
 }
